8734be413e4103b2ea3eaad4defcad0595474689
Jiri Prochazka <ji.prochazka@gmail.com>
2016-06-26 21:47:49 +0400

    Added support for Entity Framework Migrations

--- src/BitsOnCoffee.Data.EntityFramework/Context/EntityFrameworkContextBase.cs
+++ src/BitsOnCoffee.Data.EntityFramework/Context/EntityFrameworkContextBase.cs
@@ -103,4 +103,40 @@ namespace BitsOnCoffee.Data.EntityFramework.Context
 		} 
 		#endregion
 	}
+
+	public abstract class EntityFrameworkContextBase<TContext> : EntityFrameworkContextBase where TContext : DbContext
+	{
+		#region ContextBase (ctor)
+		public EntityFrameworkContextBase(string nameOrConnectionString)
+			: base(nameOrConnectionString)
+		{
+
+		}
+		#endregion
+
+		#region SaveChanges
+		public override int SaveChanges()
+		{
+			try
+			{
+				return base.SaveChanges();
+			}
+			catch (DbEntityValidationException ex)
+			{
+				StringBuilder sb = new StringBuilder();
+
+				foreach (var failure in ex.EntityValidationErrors)
+				{
+					sb.AppendFormat("{0} failed validation\n", failure.Entry.Entity.GetType());
+					foreach (var error in failure.ValidationErrors)
+					{
+						sb.AppendFormat("- {0} : {1}", error.PropertyName, error.ErrorMessage);
+					}
+				}
+
+				throw new DbEntityValidationException("Entity Validation Failed - errors follow:\n" + sb.ToString(), ex);
+			}
+		}
+		#endregion
+	}
 }
